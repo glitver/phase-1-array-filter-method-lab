898acea04d43870531e66a99ff30fe6294f6535a
+ <+>
2024-03-27 11:05:23 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(drivers => drivers.toLowerCase() === name.toLowerCase());
+  }
+  
+
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(drivers => drivers.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  
+
+  function matchName(drivers, name) {
+    return drivers.filter(drivers => drivers.name.toLowerCase() === name.toLowerCase());
+  }
